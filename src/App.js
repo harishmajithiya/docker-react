98c5d1d738c9cb376b17bf1519739a1aa40f40fb
edureka <edureka@localhost.localdomain>
2021-08-09 09:57:38 +0530

    AWS Elastic deployment

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Now! Now!
+          Learn React Now! Now! Welcome to AWS Elastic beanstock
         </a>
       </header>
     </div>
